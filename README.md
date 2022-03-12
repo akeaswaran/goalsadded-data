ae9c919b772776619c51909e845788fab2e73025
Akshay Easwaran <akeaswaran@me.com>
2022-03-12 00:40:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# G+ Data
+---
+
+All data retrieved using the [American Soccer Analysis API](https://app.americansocceranalysis.com/api/v1/__docs__/#).
+
+Credit for all data also goes to [American Soccer Analysis](https://americansocceranalysis.com). This repo is meant to be a weekly cache of G+ percentiles subdivided by each applicable action type at every position. You can retrieve this same data via ASA's [Shiny app](https://api.americansocceranalysis.com), but I wanted something more automated for future projects.
---
+++ asa_retrieve.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import json
+
+def retrieve_data(split_by_game = False, split_by_seasons = True):
+    gplus_data = pd.DataFrame()
+    for yr in range(2013, 2023):
+        print(f"Grabbing data for field players in {yr} with params: split_by_game = {split_by_game}, split_by_seasons = {split_by_seasons}")
+        url = f"https://app.americansocceranalysis.com/api/v1/mls/players/goals-added?season_name={yr}&split_by_teams=true&split_by_seasons={split_by_seasons}&split_by_games={split_by_game}"
+        tmp = pd.read_json(url)
+        tmp['season'] = yr
+        
+        print(f"Grabbing data for GKs in {yr} with params: split_by_game = {split_by_game}, split_by_seasons = {split_by_seasons}")
+        gk_url = f"https://app.americansocceranalysis.com/api/v1/mls/goalkeepers/goals-added?season_name={yr}&split_by_teams=true&split_by_seasons={split_by_seasons}&split_by_games={split_by_game}"
+        tmp_gk = pd.read_json(gk_url)
+        tmp_gk["general_position"] = "GK"
+        tmp_gk['season'] = yr
+        
+        gplus_data = gplus_data.append(tmp, ignore_index=True)
+        gplus_data = gplus_data.append(tmp_gk, ignore_index=True)
+        
+    json_gk_expl_txt = json.loads(gplus_data.explode('data').to_json(orient="records"))
+    return pd.json_normalize(json_gk_expl_txt)
+
+def percentiles(base, position, action_type, year):
+    slice_gplus = base[(base.general_position == position) & (base["data.action_type"] == action_type) & (base.season == year)]
+    if (len(slice_gplus) == 0):
+        # print(f"no data for Combo of {position} / {action_type}")
+        return pd.DataFrame()
+
+    print(f"Compiling data for combo of {position} / {action_type} / {year}") 
+    data = slice_gplus["data.goals_added_raw"]
+    return pd.DataFrame({ "position" : position, "action_type" : action_type, "season" : year, "pct" : base_range, "value" : data.quantile(base_range)})
+
+print(f"Retriving fresh G+ data from ASA...") 
+gplus_expl_flat = retrieve_data(False, True)
+
+print(f"Found {len(gplus_expl_flat)} total rows from ASA, parsing...") 
+base_range = np.linspace(0.01, 1.00, 100)
+years = gplus_expl_flat["season"].unique().tolist()
+print(f"Found {len(years)} unique seasons in data set: {years}")
+action_types = gplus_expl_flat["data.action_type"].unique().tolist()
+print(f"Found {len(action_types)} unique action types in data set: {action_types}")
+positions = gplus_expl_flat["general_position"].unique().tolist()
+print(f"Found {len(positions)} unique positions in data set: {positions}")
+
+print(f"Generating seasonal composites...") 
+percentile_composite = pd.DataFrame()
+for t in action_types:
+    for p in positions:
+        for y in years:
+            df = percentiles(gplus_expl_flat, p, t, y)
+            percentile_composite = percentile_composite.append(df, ignore_index=True)
+
+print(f"Saving {len(percentile_composite)} seasonal percentiles to disk...")
+percentile_composite.to_csv('./data/season-g+-pct.csv', index=False)
+print(f"Generated {len(percentile_composite)} seasonal percentiles and saved to disk.") 
+
+print(f"Grabbing players for look-up table...") 
+player_list = gplus_expl_flat[gplus_expl_flat.player_id.notna() == True].player_id.unique().tolist()
+print(f"Found {len(player_list)} players in G+ dataset, parsing...")
+
+player_data = pd.DataFrame()
+chunks = np.array_split(player_list, 50)
+for i in range(0, len(chunks)):
+    chunk = chunks[i]
+    id_list = ','.join(chunk)
+    print(f"Grabbing player data using ids chunk {i} of {len(chunks)}...")
+    url = f"https://app.americansocceranalysis.com/api/v1/mls/players?player_id={id_list}"
+    tmp = pd.read_json(url)
+    player_data = player_data.append(tmp, ignore_index=True)
+
+print(f"Found {len(player_data)} ASA player records, dropping dupes...")
+player_data.drop_duplicates('player_id',inplace=True)
+print(f"Found {len(player_data)} unique ASA player records, slimming and saving to disk...")
+slim_set = player_data[['player_id', 'player_name']]
+slim_set.to_csv('./data/player_lookup.csv',index=False)
+print(f"Saved lookup table of {len(player_data)} player records to disk.")
